2d7939c4971ac5bf11e36cbdafb8cc0318f0c3e3
cnkaptan <l5208019@std.yildiz.edu.tr>
2015-06-06 15:36:42 +0200

    changed description of weather item,added week of day and fixed first letter

--- app/src/main/java/com/cihankaptan/weather/android/adapter/WeatherListAdapter.java
+++ app/src/main/java/com/cihankaptan/weather/android/adapter/WeatherListAdapter.java
@@ -12,6 +12,8 @@ import com.cihankaptan.weather.android.model.WeeklyItemEntity;
 import com.cihankaptan.weather.android.model.WeeklyWeatherResponse;
 import com.squareup.picasso.Picasso;
 
+import java.util.Calendar;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
@@ -34,6 +36,8 @@ public class WeatherListAdapter extends BaseAdapter {
     public final String CELCIUS = "celcius";
     public final String FAHRENEIT = "fahreneit";
     public final String KELVIN = "kelvin";
+    private String description;
+    private int anInt;
 
     public WeatherListAdapter(WeeklyWeatherResponse weeklyWeatherResponse, String degreeType) {
         this.weeklyWeatherResponse = weeklyWeatherResponse;
@@ -75,10 +79,41 @@ public class WeatherListAdapter extends BaseAdapter {
 
         }
         weatherDegree.setText(degree+"\u00B0");
-        weatherCondition.setText(getItem(position).getWeather().get(0).getDescription());
+
+        long timestamp = Long.valueOf(getItem(position).getDt())*1000l;
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(timestamp);
+        anInt = cal.get(Calendar.DAY_OF_WEEK);
+        description = getItem(position).getWeather().get(0).getDescription();
+        String[] items = description.split(" ");
+
+        StringBuilder builder = new StringBuilder();
+        for (String str: items){
+            char[] stringArray = str.trim().toCharArray();
+            stringArray[0] = Character.toUpperCase(stringArray[0]);
+            str = new String(stringArray);
+            builder.append(str).append(" ");
+        }
+        builder.append(getWeekOfDay(anInt));
+
+        weatherCondition.setText(builder.toString());
 
 
 
         return view;
+    }
+
+
+    String getWeekOfDay(int day){
+        switch (day){
+            case 1: return "Sunday";
+            case 2: return "Monday";
+            case 3: return "Tuesday";
+            case 4: return "Wednesday";
+            case 5: return "Thursday";
+            case 6: return "Friday";
+            case 7: return "Saturday";
+            default: return "";
+        }
     }
 }
